942fe66b99e2544a149b760a1e8cdd0bbaddcf76
Margarita <47417976+MargoSolo@users.noreply.github.com>
2023-04-18 11:39:32 +0300

    easy

---
+++ easy_report.R
@@ -0,0 +1,37 @@
+# Check if libraries are installed and install if necessary   
+needed_packages <- c("report",  'readxl') 
+
+for (package in needed_packages) {
+  if (!require(package, character.only = TRUE)) {
+    install.packages(package, dependencies = TRUE)
+    library(package, character.only = TRUE)
+  }
+}
+
+# Load required libraries 
+library(readxl)
+library(report) # to report results 
+
+#Sets the working directory to the specified path. 
+setwd('/../DataMedLinkedIn')
+
+# Load dataset
+library(readxl)
+data <- read_excel("data.xlsx")
+
+# Define variable names
+# sbreath: Shortness of breath
+# sroh: Self-reported health status (rated from E to P)
+# SAD: Systolic blood pressure
+# Chol: Cholesterol
+# bmi: Body mass index
+# age: Age
+
+# Run t-test to compare BMI between those with and without shortness of breath
+t.test(bmi ~ sbreath, data = data) %>% report()
+
+# Run correlation test to examine the relationship between BMI and age
+cor.test(bmi, age, data = data) %>% report()
+
+# Run ANOVA to investigate the relationship between SAD, Chol, and BMI
+aov(SAD ~ Chol + bmi, data = data) %>% report()
